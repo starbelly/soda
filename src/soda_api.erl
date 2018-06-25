12691def64c2374f380f2388cd76e285208b0508
Bryan Paxton <starbelly@pobox.com>
2018-06-24 20:31:42 -0500

    nif load fixups

--- src/soda_api.erl
+++ src/soda_api.erl
@@ -15,13 +15,32 @@
          aead_xchacha20poly1305_ietf_keygen/0
 ]).
 
-% Random
+% Random data generation
 -export([randombytes/1]).
 
 -on_load(init/0).
 
 init() ->
-    load_soda_api().
+  SoName = case code:priv_dir(?APPNAME) of
+        {error, bad_name} ->
+            case filelib:is_dir(filename:join(["..", priv])) of
+                true ->
+                    filename:join(["..", priv, ?LIBNAME]);
+                _ ->
+                    filename:join([priv, ?LIBNAME])
+            end;
+        Dir ->
+            filename:join(Dir, ?LIBNAME)
+    end,
+    erlang:load_nif(SoName, 0).
+
+crypto_pwhash(_Password, _Salt)                                             -> erlang:nif_error(nif_not_loaded).
+crypto_pwhash_str(_Password)                                                -> erlang:nif_error(nif_not_loaded).
+crypto_pwhash_str_verify(_Hash,_Password)                                   -> erlang:nif_error(nif_not_loaded).
+crypto_randombytes(_RequestedSize)                                          -> erlang:nif_error(nif_not_loaded).
+crypto_aead_xchacha20poly1305_ietf_keygen()                                 -> erlang:nif_error(nif_not_loaded).
+crypto_aead_xchacha20poly1305_ietf_encrypt(_Msg, _Ad, _Nonce, _Key)         -> erlang:nif_error(nif_not_loaded).
+crypto_aead_xchacha20poly1305_ietf_decrypt(_Ciphered, _AD, _Nonce, _Key)    -> erlang:nif_error(nif_not_loaded).
 
 -spec pwhash(iodata(), binary()) -> {ok, binary()} | {error, term()}.
 pwhash(Str, Salt) ->
@@ -65,26 +84,3 @@ aead_xchacha20poly1305_ietf_decrypt(CT, AD, Nonce, Key) when
       is_binary(CT) andalso is_binary(AD) andalso is_binary(Nonce) andalso
       is_binary(Key) ->
     crypto_aead_xchacha20poly1305_ietf_decrypt(CT, AD, Nonce, Key).
-
-%%% @private
-crypto_pwhash(_Password, _Salt)                                             -> erlang:nif_error(nif_not_loaded).
-crypto_pwhash_str(_Password)                                                -> erlang:nif_error(nif_not_loaded).
-crypto_pwhash_str_verify(_Hash,_Password)                                   -> erlang:nif_error(nif_not_loaded).
-crypto_randombytes(_RequestedSize)                                          -> erlang:nif_error(nif_not_loaded).
-crypto_aead_xchacha20poly1305_ietf_keygen()                                 -> erlang:nif_error(nif_not_loaded).
-crypto_aead_xchacha20poly1305_ietf_encrypt(_Msg, _Ad, _Nonce, _Key)         -> erlang:nif_error(nif_not_loaded).
-crypto_aead_xchacha20poly1305_ietf_decrypt(_Ciphered, _AD, _Nonce, _Key)    -> erlang:nif_error(nif_not_loaded).
-
-load_soda_api() -> 
-  SoName = case code:priv_dir(?APPNAME) of
-        {error, bad_name} ->
-            case filelib:is_dir(filename:join(["..", priv])) of
-                true ->
-                    filename:join(["..", priv, ?LIBNAME]);
-                _ ->
-                    filename:join([priv, ?LIBNAME])
-            end;
-        Dir ->
-            filename:join(Dir, ?LIBNAME)
-    end,
-    erlang:load_nif(SoName, 0).
