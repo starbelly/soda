b6216de82728b7a5281ec3230a4af4adefa29730
Bryan Paxton <starbelly@pobox.com>
2018-07-26 22:50:25 -0500

    docs and fixups

--- src/soda.erl
+++ src/soda.erl
@@ -1,4 +1,7 @@
-%%% @doc module soda provides a refreshing interface to soda_api
+%%% @author Bryan Paxton <starbelly@pobox.com>
+%%% @doc soda module is the default interface which intends to provide 
+%%% a simple and inuitive interface to soda_api. For advanced usage 
+%%% please refer to soda_api.
 %%% @end.
 -module(soda).
 
@@ -19,7 +22,12 @@
 
 to_bin(List) -> erlang:list_to_binary(List).
 
-
+%% @doc aead_encrypt/0 encrypt a message with additional data 
+%% The aead_encrypt/2 function returns an encrypted binary created from the
+%% message `Msg'  and non-confidential additional data `Ad'. 
+%% The additional data may be 0 byte if no additional data is required.
+%% Returns the ciphered text, 192 bit nonce and a secret key. 
+%% @end
 -spec aead_encrypt(binary(), binary()) -> {ok, binary(), binary(), binary()}.
 aead_encrypt(Msg, Ad) when is_list(Msg) andalso is_list(Ad) ->
      aead_encrypt(to_bin(Msg), to_bin(Ad));
@@ -33,6 +41,10 @@ aead_encrypt(Msg, Ad) when is_binary(Msg) andalso is_binary(Ad) ->
     C = soda_api:aead_xchacha20poly1305_ietf_encrypt(Msg, Ad, N, K),
     {ok, C, N, K}.
 
+%% @doc aead_encrypt/4 encrypt a message with additional data 
+%% The aead_decrypt/4 function returns a decrypted message using the supplied
+%% cipher text, non-confidential Addition Data, Nonce, and Key. 
+%% @end
 -spec aead_decrypt(binary(), binary(), binary(), binary()) -> {ok, binary()}.
 aead_decrypt(C, Ad, N, K)  when is_binary(C)  andalso is_binary(Ad) andalso
                                 is_binary(N) andalso size(N) == 24 andalso
@@ -40,6 +52,10 @@ aead_decrypt(C, Ad, N, K)  when is_binary(C)  andalso is_binary(Ad) andalso
     M = soda_api:aead_xchacha20poly1305_ietf_decrypt(C, Ad, N, K),
     {ok, M}.
 
+%% @doc nonce/1 generate a nonce for supported algorithms.
+%% The following nonce types are currently supported:
+%%   `aead_xchacha20poly1305_ietf'
+%% @end
 -spec nonce(atom()) -> binary() | {error, term()}.
 nonce(NonceType) when is_atom(NonceType) ->
     case maps:get(NonceType, ?NONCE_SIZES, none) of
@@ -47,15 +63,25 @@ nonce(NonceType) when is_atom(NonceType) ->
         Size -> soda_api:randombytes(Size)
     end.
 
+%% @doc 
+%% Creates a hashed password suitable for storage (e.g., RDMBS, Mnesia, etc.). 
+%% All parameters needed to verify the password are stored in the returned
+%% binary. 
+%% @end
 -spec password_hash(binary()) -> {ok, binary()} | {error, term()}.
 password_hash(Str) ->
     soda_api:pwhash_str(Str).
 
+%% @doc 
+%% Verifies a password against a hashed password as created by `password_hash/1'
+%% @end
 -spec password_verify(binary(), binary()) -> boolean().
 password_verify(HashStr, Str) ->
     soda_api:pwhash_str_verify(HashStr, Str).
 
+%% @doc
+%% Generate a binary consisting a sequence of `N' unpredictable random bytes.
+%% @end
 -spec rand(non_neg_integer()) -> binary().
 rand(N) when N >= 0 ->
     soda_api:randombytes(N).
-
--- src/soda_api.erl
+++ src/soda_api.erl
@@ -1,4 +1,7 @@
-%%% @doc module soda_api provides bindings to libsodium for Erlang
+%%% @author Bryan Paxton <starbelly@pobox.com>
+%%% @doc The soda_api is the lowest level interface of the soda library. It is
+%%% highly recommended to review the official libsodium documentation before
+%%% making use of this module.  
 %%% @end.
 -module(soda_api).
 
@@ -7,29 +10,52 @@
 
 % AEAD Constructions
 -export([
-         pwhash/2,
-         pwhash_str/1,
-         pwhash_str_verify/2,
-         sign_keypair/0,
-         sign_detached/2,
-         sign_verify_detached/3,
          aead_xchacha20poly1305_ietf_encrypt/4,
          aead_xchacha20poly1305_ietf_decrypt/4,
          aead_xchacha20poly1305_ietf_keygen/0
 ]).
 
-% Random data generation
+% Password Hashing and Key Derivation
+-export([
+         pwhash/2,
+         pwhash_str/1,
+         pwhash_str_verify/2
+]).
+
+% Public Key Signatures
+-export([
+         sign_keypair/0,
+         sign_detached/2,
+         sign_verify_detached/3
+]).
+
+% Random Data Generation
 -export([randombytes/1]).
 
 -on_load(init/0).
 
--spec pwhash(iodata(), binary()) -> {ok, binary()} | {error, term()}.
-pwhash(Str, Salt) ->
-    crypto_pwhash(Str, Salt).
+%% @doc pwhash/2 key derivation
+%% The pwhash/2 function derives a key from a `Passwd' whose length is at least 
+%% `pwhash_PASSWD_MIN' bytes and a `Salt' whose size is `crypto_pwhash_SALTBYTES' bytes. 
+%% Returns a binary with a minumum length of at least `crypto_pwhash_BYTES_MIN'
+%% and at most `crypto_pwhash_BYTES_MAX'.
+%% @end
+-spec pwhash(binary(), binary()) -> {ok, binary()} | {error, term()}.
+pwhash(Passwd, Salt) ->
+    crypto_pwhash(Passwd, Salt).
 
--spec pwhash_str(iodata()) -> {ok, iodata()} | {error, term()}.
-pwhash_str(Password) ->
-    case crypto_pwhash_str(Password) of
+%% @doc pwhash_str/1 hash a password for storage
+%%  The pwhash_str/1 function is used for generating hashed passwords that are
+%%  suitable for storage (e.g., RDBMS, Menesia, etc.) 
+%%% Specifically, the `Passwd' which shall have a minimum length of
+%%% `crypto_pwhash_PASSWD_MIN' is hashed using a memory-hard, CPU-intensive hash
+%%% function applied to the password passwd of len. The salt required for the
+%%% hashing along with all parameters needed to verify a password against the
+%%% hash is stored in the returned binary. 
+%% @end
+-spec pwhash_str(binary()) -> {ok, binary()} | {error, term()}.
+pwhash_str(Passwd) ->
+    case crypto_pwhash_str(Passwd) of
         {ok, Str} ->
             [X, _] =  binary:split(Str, <<0>>),
             {ok, X};
@@ -37,42 +63,70 @@ pwhash_str(Password) ->
             {error, Reason}
     end.
 
+%% @doc pwhash_str_verify/2 verify a hashed password
+%% The pwhash_str_verify/2 function verifies the provided `Passwd' against a
+%% supplied `HashStr. `Passwd' should be at least `crypto_pwhash_PASSWD_MIN'
+%% @end
 -spec pwhash_str_verify(binary(), iodata()) -> boolean().
 pwhash_str_verify(HashStr, Password) ->
     crypto_pwhash_str_verify(HashStr, Password).
 
+%% @doc randombytes/1 generate random data
+%% Creates and returns a binary with a size of `N' filled with an unpredictable sequence of bytes.
+%% @end
 -spec randombytes(non_neg_integer()) -> binary().
 randombytes(N) when N >= 0 ->
     crypto_randombytes(N).
 
+%% @doc sign_keypair/0 Create key pair suitable for public signatures. 
+%% The sign_keypair/0 function randomly generates a secret key with a size of
+%% `crypto_sign_SECRETKEYBYTES' bytes and a corresponding public key with a size
+%% of crypto_sign_PUBLICKEYBYTES
+%% @end
 -spec sign_keypair() -> {binary(), binary()}.
 sign_keypair() ->
     crypto_sign_keypair().
 
--spec sign_detached(M, SK) -> DS
+%%% @doc sign_detached/2 
+%%% The sign_detached/2 function signs a the message`M' using
+%%% the secret key `Sk' that is a minimum of `crypto_sign_SECRETKEYBYTES' bytes.
+%%% @end
+-spec sign_detached(M, Sk) -> {ok, Ds} | {error, failed_verification}
     when
       M  :: iodata(),
-      SK :: binary(),
-      DS :: binary().
-sign_detached(M, SK) ->
-    crypto_sign_detached(M, SK).
+      Sk :: binary(),
+      Ds :: binary().
+sign_detached(M, Sk) ->
+    crypto_sign_detached(M, Sk).
 
--spec sign_verify_detached(SIG, M, PK) -> {ok, M} | {error, failed_verification}
+%%% @doc sign_verify_detached/3 validate a signature.
+%%% The sign_verify_detached/3 function verifies the signature `Sig' is valid for a
+%%% the given message `M' using the public key `Pk'.
+%%% @end
+-spec sign_verify_detached(Sig, M, Pk) -> {ok, M} | {error, failed_verification}
     when
-      SIG :: binary(),
+      Sig :: binary(),
       M   :: iodata(),
-      PK  :: binary().
-sign_verify_detached(SIG, M, PK) ->
-    case crypto_sign_verify_detached(SIG, M, PK) of
+      Pk  :: binary().
+sign_verify_detached(Sig, M, Pk) ->
+    case crypto_sign_verify_detached(Sig, M, Pk) of
         true -> {ok, M};
         false -> {error, failed_verification}
     end.
 
-
+%% ----------------------
+%% @doc aead_xchacha20poly1305_ietf_keygen/0 generates a random key is
+%% equivalent to calling `randombytes/1' with `aead_xchacha20poly1305_ietf_KEYBYTES'
+%% @end
 -spec aead_xchacha20poly1305_ietf_keygen() -> binary() | {error, term()}.
 aead_xchacha20poly1305_ietf_keygen() ->
     crypto_aead_xchacha20poly1305_ietf_keygen().
 
+%% ----------------------
+%% @doc aead_xchacha20poly1305_ietf_encrypt/4 encrypts `Message' with additional data
+%% `AD' using `Key' and `Nonce'. Returns the encrypted message followed by
+%% `aead_chacha20poly1305_ietf_ABYTES/0' bytes of MAC.
+%% @end
 -spec aead_xchacha20poly1305_ietf_encrypt(binary(), binary(), binary(),
                                           binary()) -> binary() | {error,
                                                                    term()}.
@@ -84,6 +138,12 @@ aead_xchacha20poly1305_ietf_encrypt(Message, AD, Nonce, Key) when
 -spec aead_xchacha20poly1305_ietf_decrypt(binary(), binary(), binary(),
                                           binary()) -> binary() | {error,
                                                                    term()}.
+
+%% @doc aead_xchacha20poly1305_ietf_decrypt/4 decrypts ciphertext `CT' with additional
+%% data `AD' using `Key' and `Nonce'. Note: `CipherText' should contain
+%% `crypto_aead_xchacha20poly1305_ietf_ABYTES' bytes that is the MAC. Returns the decrypted
+%% message.
+%% @end
 aead_xchacha20poly1305_ietf_decrypt(CT, AD, Nonce, Key) when
       is_binary(CT) andalso is_binary(AD) andalso is_binary(Nonce) andalso
       is_binary(Key) ->
@@ -105,7 +165,7 @@ init() ->
     erlang:load_nif(SoName, 0).
 
 
-% NIF stubs
+%%% NIF stubs
 crypto_pwhash(_Password, _Salt)
     -> erlang:nif_error(nif_not_loaded).
 
